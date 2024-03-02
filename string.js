4a601d4d1f155bb00c4460627ec6e15a5fe8a94b
Shalindra-coder <156834865+Shalindra-coder@users.noreply.github.com>
2024-03-02 04:22:44 -0800

    Add files via upload

---
+++ string.js
@@ -0,0 +1,58 @@
+//learnt strings
+// let str = "shalindra aporiya";
+// let str2 = 'aporiya';
+// console.log(str.length);
+// console.log(str[5]);
+//template literals
+// let specialstring = `this is a template literals`;
+// console.log(specialstring);
+// let obj = {
+//     item: "Pen",
+//     price: 34,
+// }
+// let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+//********************* 
+//escape cherecter use
+// let str = "shalindra\naporiya";
+// let str1 = "shalindra\taporiya";
+// console.log(str1);
+// console.log(str.length);
+//******************************************
+//strings method use 
+// let str = "shalindraaporiya";
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// let str2 = "    aporiya   shalindra  23";
+// console.log(str2.trim());
+//************************** 
+//slice methods
+// let str = "1234556667";
+// console.log(str.slice(5,9));
+// let str1 = "shalindraaporiya";
+// console.log(str1.slice(2,8));
+//*********************** 
+//two stings connect
+//concat methods
+// let str1 = "shalindr";
+// let str2 = "aporiya";
+// console.log(str1.concat(str2));
+// console.log("i am learning coding from"+str1+(str2));
+//***************************** 
+//replace methods use
+// let str = "shalindra";
+// console.log(str.replace("s","p"));
+// //find index value 
+// console.log(str.charAt(4));
+//*****************************
+// let str1 = "@";
+// let str2 = "shalindraaporiya";
+// let str3 = "16";
+// console.log(str1+str2+str3);
+let fullname = prompt("enter user without spce");
+let username = "@"+fullname+fullname.length;
+console.log(username);
+
+
+
+
