8ed58c1669558025de3cee54931fccd4fc7278f9
Shalindra-coder <156834865+Shalindra-coder@users.noreply.github.com>
2024-05-17 09:30:02 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,63 @@
+// let div = document.getElementById("div");
+// console.log(div);
+
+// let div = document.getElementById("div");
+// console.dir(div);
+
+
+// let elements = document.getElementsByClassName("h2-class");
+// console.log(elements);
+// console.dir(elements);
+
+// let elements = document.getElementsByTagName("p");
+// console.log(elements);
+// console.dir(elements);
+// let firstelement = document.querySelector("p");
+// console.log(firstelement);
+
+
+// let Allelement = document.querySelectorAll("p");
+// console.log(Allelement);
+
+
+// let element = document.querySelector(".h2-class");
+// console.log(element);
+
+
+// let id = document.querySelectorAll("#div");
+// console.log(id);
+
+
+// let id1 = document.querySelector("#div");
+// console.log(id1);
+
+
+// let class1 = document.querySelectorAll(".h2-class");
+// console.log(class1);
+
+
+
+// let firstelement = document.querySelector("p");
+// console.log(firstelement);
+
+// let children = document.querySelector("div").children;
+// console.log(children);
+
+// let firstchild = document.querySelector("div").firstchildren;
+// console.log(firstchild);
+
+
+// let lastChild = document.querySelector("div").lastChild;
+// console.log(lastChild);
+
+// let div = document.querySelector("div");
+// console.dir(div).innerText;
+
+
+// let heading = document.querySelector("h1");
+// console.dir(heading).textContant;
+// let heading = document.querySelector("h2");
+// console.dir(heading).innerText = "from apna college";
+
+
+let p = document.querySelector('p').innerHTML = "hallo world";
