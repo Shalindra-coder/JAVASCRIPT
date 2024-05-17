428162647e1cbdf7b28cf74a212c0fbee5b66eea
Shalindra-coder <156834865+Shalindra-coder@users.noreply.github.com>
2024-05-17 10:17:42 +0530

    Add files via upload

---
+++ type consversion.js
@@ -0,0 +1,54 @@
+// let a = 123;
+// let b = String(123);
+// console.log(a,(typeof a))
+//  console.log(b, (typeof b))
+// let bolean = true;
+// let str = String(true)
+// console.log(bolean,(typeof bolean))
+// console.log(str,(typeof str))
+
+//  let arr = [1,2,3,4];
+//  let arr1 = String([1,2,3,4])
+ 
+//  console.log(arr.length,(typeof arr))
+//  console.log(arr1.length,(typeof arr1))
+ 
+//  let a = 45;
+//  console.log(a.toString())
+
+
+// let str = "1234"
+// str1 = Number("1234")
+// console.log(str,(typeof str))
+// console.log(str1, (typeof str1))
+// let h = "1223d"
+// let p = Number("1234d")
+// console.log(h,(typeof h))
+// console.log(p, (typeof p))
+
+// let a = true;
+// let b = Number(true);
+// console.log(a, (typeof a))
+// console.log(b,(typeof b))
+
+// number
+// strings
+// boolean
+// null
+// undefined
+// bigint
+// symboll
+
+
+// let a = '1233';
+// let b = parseInt( '1233')
+// let c = parseFloat("12.33")
+// console.log(a,(typeof a))
+// console.log(b,(typeof b))
+// console.log(c.toFixed(7),(typeof c))
+
+// type coercion
+// let str = "123"
+// let a = 123;
+// console.log(str+a);
+
