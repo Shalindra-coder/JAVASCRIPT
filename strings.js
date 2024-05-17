a98201d6791ca11e1d7e686e9d25f11c0d1da7dd
Shalindra-coder <156834865+Shalindra-coder@users.noreply.github.com>
2024-05-17 09:23:55 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,25 @@
+let str = "shalindraaporiya";
+let newstr = str.toUpperCase();
+console.log(newstr);
+let size = 0;
+for(let i of str){
+    console.log(i);
+    size++;
+}
+console.log(size);
+
+
+// var sha = str.charAt(6);
+// console.log(sha);
+
+// var sha = str.charCodeAt(3);
+// console.log(sha);
+
+// var sha = str.at(8);
+// console.log(sha);
+
+// var sha = str.slice[2,8];
+// console.log(sha);
+
+// var sha = str.(3);
+// console.log(sha);
