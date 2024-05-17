0f70be2997048ca4145cbb7f27bbafdc959a1b65
Shalindra-coder <156834865+Shalindra-coder@users.noreply.github.com>
2024-05-17 09:31:22 +0530

    Add files via upload

---
+++ errorhandling.js
@@ -0,0 +1,17 @@
+let a = 12;
+let b= 10;
+console.log("a",  a);
+console.log("b", b);
+console.log("a+b", a+b);
+console.log("a+b", a+b);
+try{
+console.log("a+b", a+c);//error
+}catch(error){
+    console.log(error);
+}
+console.log("a+b", a+b);
+console.log("a+b", a+b);
+console.log("a+b", a+b);
+console.log("a+b", a+b);
+console.log("a+b", a+b);
+console.log("a+b", a+b);
